bc8dfaa560936761170236edd14460d6c4ac411a
excelsi0r <nuno_mgl@hotmail.com>
2016-11-28 22:15:22 +0000

    added header

---
+++ ftp.h
@@ -0,0 +1,17 @@
+#ifndef FTP_H
+#define TFP_H
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define MAX_BUF 256
+
+#endif
